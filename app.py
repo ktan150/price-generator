14488e09c66b0b4e192c74ecfba84635b5c62086
ktan150 <99799948+ktan150@users.noreply.github.com>
2022-04-05 18:44:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ import joblib
 @app.route("/", methods=["GET", "POST"]) 
 def index():
     if request.method == "POST":
-        longitude = request.form.get("longitude")
         latitude = request.form.get("latitude")
+        longitude = request.form.get("longitude")
         microwave = request.form.get("microwave")
         smoking_allowed = request.form.get("smoking_allowed")
         p_livingroom = request.form.get("p_livingroom")
@@ -32,7 +32,7 @@ def index():
         pred = model.predict([[float(latitude), float(longitude),int(microwave),int(smoking_allowed),int(p_livingroom)]])
         print(pred)
         pred = pred[0]
-        s = "The predicted price for your apartment is " + str(pred)
+        s = "The predicted price for your apartment is $" + str(pred)
         return(render_template("index.html", result=s))
     else: 
         return(render_template("index.html", result="Please enter the required information to estimate a price to charge your guests"))
